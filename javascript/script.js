57a266ff032a27b20747bdf32edd6ddd6dfa8c6b
Joel Quispe <joelalessandro@live.com>
2020-11-03 16:46:36 -0400

    diseño login

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+
+let usuario = document.getElementById("txtUsuario");
+let contrasena = document.getElementById("txtContrasena");
+
+function obtenerdatos() {
+    alert("EL USUARIO ES: "+usuario.value+"\n"+"LA CONTRASEÑA ES: "+contrasena.value);
+}
